08d350feddee05c38c88f218d1a4ad7289d68a03
Brad Johnson <climatebrad@gmail.com>
2024-07-31 12:25:01 -0400

    upgrade ids to bigints

---
+++ db/migrate/20240731154203_upgrade_ids_to_bigints.rb
@@ -0,0 +1,43 @@
+class UpgradeIdsToBigints < ActiveRecord::Migration[7.2]
+  def change
+    change_column :blogs, :id, :bigint
+
+    change_column :contents, :id, :bigint
+    change_column :contents, :user_id, :bigint
+    change_column :contents, :parent_id, :bigint
+    change_column :contents, :blog_id, :bigint
+
+    change_column :contents_tags, :content_id, :bigint
+    change_column :contents_tags, :tag_id, :bigint
+
+    change_column :feedback, :id, :bigint
+    change_column :feedback, :user_id, :bigint
+    change_column :feedback, :article_id, :bigint
+
+    change_column :pings, :id, :bigint
+    change_column :pings, :article_id, :bigint
+
+    change_column :post_types, :id, :bigint
+
+    change_column :redirects, :id, :bigint
+    change_column :redirects, :content_id, :bigint
+    change_column :redirects, :blog_id, :bigint
+
+    change_column :resources, :id, :bigint
+    change_column :resources, :content_id, :bigint
+    change_column :resources, :blog_id, :bigint
+
+    change_column :sessions, :id, :bigint
+
+    change_column :sidebars, :id, :bigint
+    change_column :sidebars, :blog_id, :bigint
+
+    change_column :tags, :id, :bigint
+    change_column :tags, :blog_id, :bigint
+
+    change_column :triggers, :id, :bigint
+
+    change_column :users, :id, :bigint
+    change_column :users, :resource_id, :bigint
+  end
+end
